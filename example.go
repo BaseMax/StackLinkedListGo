0b4c7c2550d695686df0b9109e41f0224ea79085
Max Base <maxbasecode@gmail.com>
2022-12-10 13:18:19 +0100

    create example.go

---
+++ example.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Create a new stack
+	stack := NewStackLinkedList()
+
+	// Push some elements
+	stack.Push(1)
+	stack.Push(2)
+	stack.Push(3)
+
+	// Print the stack
+	stack.Print()
+
+	// Pop the top element
+	fmt.Println(stack.Pop())
+
+	// Print the stack
+	stack.Print()
+
+	// Get the top element
+	fmt.Println(stack.Peek())
+
+	// Get the size of the stack
+	fmt.Println(stack.Size())
+
+	// Check if the stack is empty
+	fmt.Println(stack.IsEmpty())
+
+	// Clear the stack
+	stack.Clear()
+
+	// Check if the stack is empty
+	fmt.Println(stack.IsEmpty())
+
+	// Print the stack
+	stack.Print()
+}
